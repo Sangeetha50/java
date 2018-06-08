a0914d3ebf4c00b3c0112f934741c4658e6e4191
Sangeetha50 <40001271+Sangeetha50@users.noreply.github.com>
2018-06-08 15:27:44 +0530

    Update duplicate.java

--- duplicate.java
+++ duplicate.java
@@ -3,46 +3,22 @@ public class duplicate
 {
   public static void main(String[] args) 
     {
-        int[] my_array = {1,7, 2, 5, 5, 6, 6, 7, 2,7,2};
-        int[] s= new int[200];
-        
- 
-        for (int i = 0; i < my_array.length-1; i++)
-        
-        {
-            for (int j = i+1; j < my_array.length; j++)
-            {
-                if ((my_array[i] == my_array[j]) && (i != j))
-                {
-                s[i]=my_array[j];
-              
-               
-                      
-                    // System.out.println("Duplicate Element : "+my_array[j]);
-                  
-                }
-            }
-        
-        }
-          for (int i = 0; i < s.length-1; i++)
+        int[] a= {1,7, 2, 5, 5, 6, 6, 7, 2,7,2};
+        int[] s= new int[a.length];
+      
+        for (int i = 0; i < a.length-1; i++)
         
         {
-            for (int j = i+1; j < s.length; j++)
+            for (int j = i+1; j < a.length;j++)
             {
-                if ((s[i] == s[j]) && (i != j))
+                if (a[i] == a[j])
                 {
-                //s[i]=my_array[j];
               
-               
-                      
-                    System.out.println("Duplicate Element : "+my_array[j]);
-                  
-                }
-                else {
-                    
-                }
-            }
-            System.out.println(s[i]);
-        }
-    }    
+                    System.out.println("Duplicate Element : "+a[j]);
+		}
+	    }
+	}
+   }
 }
+ 
+        
