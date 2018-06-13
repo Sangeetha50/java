2716918b2d9ecef499d0eb4ea62deed8685ea141
Sangeetha50 <40001271+Sangeetha50@users.noreply.github.com>
2018-06-13 12:58:40 +0000

    Create Stringduplicates.java

---
+++ Stringduplicates.java
@@ -0,0 +1,30 @@
+public class Program
+{
+	public static void main(String[] args) {
+	    int i,j,l;
+	    int k=0;
+	    int count=0;
+	    String output="";
+	    String str="Sevvandhi";
+	    for(i=0;i<str.length();i++){
+	        for(j=0;j<str.length();j++){
+	            if(i != j){
+    	            if(str.charAt(i) == str.charAt(j)){
+    	                    count ++;
+    	                if(output.length() == 0) 
+    	                    output += str.charAt(j);
+    	                else
+    	                    for(l=0;l<output.length();l++){
+    	                        if(output.charAt(l)!= str.charAt(j))
+    	                            output += str.charAt(j);
+    	                    }
+    	                }
+	            }
+	        }
+	    }
+	    for(i=0;i<output.length();i++){
+	        System.out.print("Duplicate character " +output.charAt(i) + " " +" And count " + count);
+	    }
+	    
+	}
+}
